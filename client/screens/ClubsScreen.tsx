64b1b245e93341675c1a287c837c3ea54a3e0c18
SlickShah <sv517@georgetown.edu>
2022-01-29 13:51:21 -0500

    Updated files in Shah Branch

--- client/screens/ClubsScreen.tsx
+++ client/screens/ClubsScreen.tsx
@@ -1,14 +1,71 @@
 import React from "react";
-import { RefreshControl, ScrollView, StyleSheet } from "react-native";
+import { FlatList, RefreshControl, ScrollView, StyleSheet } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
 
+const sample: Posting[] = [
+  {
+    id: "1",
+    title: "Club 1",
+    description: "Description 1",
+    category: "Category 1",
+    status: "Status 1",
+    affliation: "Affliation 1",
+  },
+  {
+    id: "2",
+    title: "Club 2",
+    description: "Description 2",
+    category: "Category 2",
+    status: "Status 2",
+    affliation: "Affliation 2",
+  },
+  {
+    id: "3",
+    title: "Club 3",
+    description: "Description 3",
+    category: "Category 3",
+    status: "Status 3",
+    affliation: "Affliation 3",
+  },
+];
+
 export default function ClubsScreen() {
+  const [refreshing, setRefreshing] = React.useState(false);
+  const renderFlatListItem = ({ item }: { item: Posting }) => {
+    return (
+      <View style={styles.flatListItem}>
+        <View style={styles.itemHeader}>
+          <Text style={styles.subTitle}>{item.title}</Text>
+          <Text style={styles.affliation}>{item.affliation}</Text>
+        </View>
+
+        <Text numberOfLines={3}>{item.description}</Text>
+      </View>
+    );
+  };
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    // TODO: Make API call to refresh data
+    setRefreshing(false);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Clubs</Text>
+      <View style={styles.subContainer}>
+        <FlatList
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+          data={sample}
+          renderItem={renderFlatListItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
@@ -26,6 +83,7 @@ const styles = StyleSheet.create({
   },
   subTitle: {
     fontSize: 20,
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
@@ -36,7 +94,24 @@ const styles = StyleSheet.create({
     flex: 1,
     borderWidth: 2,
     padding: 10,
-    marginTop: 20,
+    margin: 10,
+    //borderColor: "green",
+  },
+  flatListItem: {
+    borderWidth: 1,
+    padding: 10,
+    margin: 5,
     borderColor: "green",
+  },
+  affliation: {
+    fontSize: 16,
+  },
+  description: {
+    fontSize: 14,
+  },
+  itemHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
   },
 });
--- client/screens/ProfileScreen.tsx
+++ client/screens/ProfileScreen.tsx
@@ -1,22 +1,103 @@
-import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootStackScreenProps, RootTabScreenProps } from '../types';
+import React from "react";
+import { FlatList, RefreshControl, ScrollView, StyleSheet } from "react-native";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+
+const sample: Posting[] = [
+  {
+    id: "1",
+    title: "First Name",
+    description: "",
+    category: "Category 1",
+    status: "Status 1",
+    affliation: "Shahryar",
+  },
+  {
+    id: "2",
+    title: "Last Name",
+    description: "",
+    category: "Category 2",
+    status: "Status 2",
+    affliation: "Valizadeh",
+  },
+  {
+    id: "3",
+    title: "Phone Number",
+    description: "",
+    category: "Category 3",
+    status: "Status 3",
+    affliation: "8001234567",
+  },
+  {
+    id: "4",
+    title: "Resume",
+    description: "",
+    category: "Category 3",
+    status: "Status 3",
+    affliation: "www.linkedin.com",
+  },
+];
 
 
+// export default function ProfileScreen({route, navigation}:RootTabScreenProps<"Profile">) {
+//   const {model} = route.params;
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>{model.firstName + ' ' + model.lastName}</Text>
+//       <Text style={styles.container}>{'Email: ' + model.email + '\n' 
+//       + 'Phone Number: ' +model.phone + '\n'
+//       + 'Resume: ' +model.resume} 
+//       </Text>
+//       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+//       <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+//     </View>
+//   );
+// }
 
-export default function ProfileScreen({route, navigation}:RootTabScreenProps<"Profile">) {
-  const {model} = route.params;
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
+
+export default function PostingsScreen() {
+  const renderFlatListItem = ({ item }: { item: Posting }) => {
+    return (
+      <View style={styles.flatListItem}>
+        <View style={styles.itemHeader}>
+          <Text style={styles.subTitle}>{item.title}</Text>
+          <Text style={styles.affliation}>{item.affliation}</Text>
+        </View>
+
+        <Text numberOfLines={3}>{item.description}</Text>
+      </View>
+    );
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{model.firstName + ' ' + model.lastName}</Text>
-      <Text style={styles.container}>{'Email: ' + model.email + '\n' 
-      + 'Phone Number: ' +model.phone + '\n'
-      + 'Resume: ' +model.resume} 
-      </Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <View style={styles.subContainer}>
+        <FlatList
+          data={sample}
+          renderItem={renderFlatListItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 }
@@ -24,16 +105,45 @@ export default function ProfileScreen({route, navigation}:RootTabScreenProps<"Pr
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    padding: 20,
   },
   title: {
+    fontSize: 26,
+    fontWeight: "bold",
+  },
+  subTitle: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  subContainer: {
+    flex: 1,
+    borderWidth: 2,
+    padding: 10,
+    margin: 10,
+    //borderColor: "green",
+  },
+  flatListItem: {
+    borderWidth: 1,
+    padding: 10,
+    margin: 5,
+    borderColor: "green",
+  },
+  affliation: {
+    fontSize: 16,
+  },
+  description: {
+    fontSize: 14,
+  },
+  itemHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 5,
   },
 });
